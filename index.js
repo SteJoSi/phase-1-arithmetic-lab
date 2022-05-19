a768296c04eb9e2dd13955fb35c04bc995da57d9
Stephanie Sison <sjsison27@icloud.com>
2022-05-18 18:11:21 -0700

    commit tree 027ec9ebda5ff94208a78d0e215170c540862a80

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+const num1 = 2
+const num2 = 31
 
-// Write your code here
+let multiply = num1 * num2
+    console.log(multiply)
+
+let random = Math.floor(Math.random() * 10)
+
+const num3 = 16
+const num4 = 4
+
+let mod =  num3 / num4
+    console.log(mod)
+
+let max = Math.max(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20)
